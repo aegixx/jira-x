35a4ed4537736b61d2e4e097b6aad34b6698985c
Bryan Stone <bstone@datapipe.com>
2017-09-17 17:03:45 -0500

    Cleanup
    Signed-off-by: Bryan Stone <bstone@datapipe.com>

--- lambda/jira-x-lambda/index.js
+++ lambda/jira-x-lambda/index.js
@@ -6,41 +6,43 @@ var Alexa = require("alexa-sdk");
 
 
 exports.handler = function(event, context) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'GetStatusIntent': function () {
-        this.emit('GetStatus');
-    },
-    'GetStatus': function () {
-        var name = this.event.request.intent.slots.name.value;
-        this.response.speak('Hello ' + name)
-            .cardRenderer('hello world', 'hello ' + name);
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest' : function() {
-        console.log('Session ended with reason: ' + this.event.request.reason);
-    },
-    'AMAZON.StopIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, get status of A.B.C. 1.2.5.' or 'alexa, update status of A.B.C. 1.2.5. to in progress");
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't get that.");
-        this.emit('AMAZON.HelpIntent');
-    }
+  'LaunchRequest': function() {
+    this.emit('SayHello');
+  },
+  'GetStatusIntent': function() {
+    this.emit('GetStatus');
+  },
+  'GetStatus': function() {
+    var project = this.event.request.intent.slots.project.value;
+    var issue = this.event.request.intent.slots.issueNum.value;
+    var phrase = util.format('%s %s', project, issue);
+    this.response.speak(phrase)
+      .cardRenderer(phrase);
+    this.emit(':responseReady');
+  },
+  'SessionEndedRequest': function() {
+    console.log('Session ended with reason: ' + this.event.request.reason);
+  },
+  'AMAZON.StopIntent': function() {
+    this.response.speak('Bye');
+    this.emit(':responseReady');
+  },
+  'AMAZON.HelpIntent': function() {
+    this.response.speak("You can try: 'alexa, ask jeera x the status of A.B.C. 1.2.5.'");
+    this.emit(':responseReady');
+  },
+  'AMAZON.CancelIntent': function() {
+    this.response.speak('Bye');
+    this.emit(':responseReady');
+  },
+  'Unhandled': function() {
+    this.response.speak("Sorry, I didn't get that.");
+    this.emit('AMAZON.HelpIntent');
+  }
 };
